809f9934d4839900cb95ad9ef3386a0da63e976a
Thirukrishnan <63901950+Thirukrishnan@users.noreply.github.com>
2022-07-25 11:21:44 +0530

    Create format-string-bof.c

---
+++ format-string-bof.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+char dum[5]="abc";
+char change[5]="dummy";
+int main()
+{
+	char temp[1337];
+	fgets(temp,6,stdin);
+	sprintf(dum,temp);
+	printf("Size is %d\n",strlen(dum));
+	printf(change);
+	if (strcmp(change,"dummy")!=0){
+printf("You won maara!");
+	}
+	else{
+		printf("Try harder!!");
+	}
+	
+}
